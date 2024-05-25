323fc0f04cbe885b15a1dbbd5de85b7eb9ac7f37
mamertoaringo <mamertoaringo9@gmail.com>
2024-05-25 22:10:03 +0800

    Nice admin sucessfully intedrated

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,9 @@
+<footer id="footer" class="footer">
+    <div class="copyright">
+        &copy; Copyright <strong><span>NiceAdmin</span></strong
+        >. All Rights Reserved
+    </div>
+    <div class="credits">
+       Modified by <a href="">Mamerto M. Aringo JR,</a>
+    </div>
+</footer>
